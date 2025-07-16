937f93f773e321263d7377c3fc9c99bc8b88719a
Your Name <luis_duarte.h@tumo.world>
2025-07-16 11:14:31 +0100

    Aula 3-0.5

--- script.js
+++ script.js
@@ -5,11 +5,21 @@ window.onload = function ()
     let playButton = document.querySelector("#play-stop-button")
     let nextButton = document.querySelector("#next-button")
     let previousButton = document.querySelector("#previous-button")
+    let scrubInput = document.querySelector("#scrub-input")
+     let bar = scrubInput.querySelector(".scrub-bar")
+     let volumeInput = document.querySelector("#volume-input")
     function changeTitle(value) 
     
     {
         title.innerText = value;
     }
+
+    function updateInputBar(value, bar) {
+
+        bar.style.transform = "scaleX(" + value /100 +")";
+    }
+
+    
     changeTitle("Bruno");
     console.log(title);
 
@@ -28,6 +38,18 @@ window.onload = function ()
     {
         console.log("Go to play/stop")
     }
+
+    scrubInput.querySelector("input").oninput = function(event) 
+    {
+        let bar = scrubInput.querySelector(".scrub-bar")
+        updateInputBar(event.target.value, bar)
+    }
+       volumeInput.querySelector("input").oninput = function(event) 
+    {
+        let bar = volumeInput.querySelector(".scrub-bar")
+        updateInputBar(event.target.value, bar)
+    }
+
 
 
 
