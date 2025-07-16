b10a09fff47b84d508a52991755c9fba80d4a05c
Your Name <luis_duarte.h@tumo.world>
2025-07-16 13:25:34 +0100

    Aula 3 fixed

--- script.js
+++ script.js
@@ -1,44 +1,53 @@
-window.onload = function ()
+window.onload = async function ()
 {
-    let title = document.querySelector("#title")
-    console.log("Hello Tumo");
-    let playButton = document.querySelector("#play-stop-button")
-    let nextButton = document.querySelector("#next-button")
-    let previousButton = document.querySelector("#previous-button")
-    let scrubInput = document.querySelector("#scrub-input")
-     let bar = scrubInput.querySelector(".scrub-bar")
-     let volumeInput = document.querySelector("#volume-input")
+    
+
+    //Pedido da internet
+let request = await fetch("data.json");
+let audioData = await request.json();
+
+
+
+    //Variáveis
+    let title = document.querySelector("#title");
+    let playButton = document.querySelector("#play-stop-button");
+    let nextButton = document.querySelector("#next-button");
+    let previousButton = document.querySelector("#previous-button");
+    let scrubInput = document.querySelector("#scrub-input");
+     let bar = scrubInput.querySelector(".scrub-bar");
+     let volumeInput = document.querySelector("#volume-input");
+    let fileInput = document.querySelector("#file-input");
+    let audio = document.querySelector("audio");
+    let currentMusic = 0;
+    
+//Fim de variáveis
+
+
+
+//Mudanças
     function changeTitle(value) 
     
     {
         title.innerText = value;
+        
+    }
+    previousButton.onclick = function() 
+    {
+        console.log("Go to previous")
+
+        if (currentMusic <= 0)
+        {
+            curentMusic = 0;
+        }
+        console.log(currentMusic);
     }
 
+    
+//Atualiza Barras de som e scrub
     function updateInputBar(value, bar) {
 
         bar.style.transform = "scaleX(" + value /100 +")";
     }
-
-    
-    changeTitle("Bruno");
-    console.log(title);
-
-    previousButton.onclick = function() 
-    {
-        console.log("Go to previous")
-    }
-
-    nextButton.onclick = function() 
-    {
-        console.log("Go to next")
-    }
-
-
-    playButton.onclick = function() 
-    {
-        console.log("Go to play/stop")
-    }
-
     scrubInput.querySelector("input").oninput = function(event) 
     {
         let bar = scrubInput.querySelector(".scrub-bar")
@@ -51,6 +60,52 @@ window.onload = function ()
     }
 
 
+    //Atualiza Botões
+    nextButton.onclick = function() 
+    {
+        console.log("Go to next")
+        console.log(currentMusic);
+    }
+
+
+    playButton.onclick = function() 
+    {
+        if(audio.paused) 
+            {
+                playAudio();
+            }
+        else
+            {
+                pauseAudio();
+            }
+
+    }
+
+    fileInput.oninput = function() 
+    {
+        console.log("aqui!");
+    }
+    function playAudio() 
+    {
+        audio.src = audioData[currentMusic].url;
+        audio.play();
+    }
+    function pauseAudio ()
+    {
+        let playIcon = document.querySelector("#icon-play");
+      let pauseIcon = document.querySelector("#icon-pause");
+        audio.pause()
+        playIcon.style.display="block";
+      pauseIcon.style.display="none";
+    }
+
+    audio.onplay = function() 
+    {
+      let playIcon = document.querySelector("#icon-play");
+      let pauseIcon = document.querySelector("#icon-pause");
+      playIcon.style.display="none";
+      pauseIcon.style.display="block";
+    }
 
 
 }
